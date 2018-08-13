506084e85f3eca69fa55ccd401fb758f0dc86f9b
Victoria Meng <nights1ststar@protonmail.com>
2018-08-13 21:30:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const currentUser = "Me"
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}`
